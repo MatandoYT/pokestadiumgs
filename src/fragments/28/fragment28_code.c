c603d3372ec9b3e9fcb0cd92fd79611abe7533d7
Matando <matthew@strelsky.net>
2025-07-30 12:53:03 -0400

    correct fragment28 vram

--- src/fragments/28/fragment28_code.c
+++ src/fragments/28/fragment28_code.c
@@ -1,4 +1,4 @@
 #include "global.h"
 
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/28/fragment28_code/func_80000020.s")
+#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/28/fragment28_code/func_8FE00020.s")
