0ac2c337d2b85b40ef3f3422bf88083a7d998e80
Matando <matthew@strelsky.net>
2025-11-30 14:11:11 -0500

    Fix typos from fragment updates

--- src/fragments/0xEE/fragment_0xEE_code.c
+++ src/fragments/0xEE/fragment_0xEE_code.c
@@ -1,4 +1,4 @@
 #include "global.h"
 
 
-#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/0xEE/fragment_0xEE_code/func_80000020.s")
+#pragma GLOBAL_ASM("asm/us/nonmatchings/fragments/0xEE/fragment_0xEE_code/func_8FE00020.s")
